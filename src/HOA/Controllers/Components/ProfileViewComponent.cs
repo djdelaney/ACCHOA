d1749e09c3b9545e89a30c27d818207b399190cd
Daniel Delaney <dan@hactar.com>
2016-11-05 22:58:03 -0400

    Add full name to header

---
+++ src/HOA/Controllers/Components/ProfileViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using HOA.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HOA.Controllers.Components
+{
+    public class ProfileViewComponent : ViewComponent
+    {
+        readonly UserManager<ApplicationUser> userManager;
+
+        public ProfileViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            try
+            {
+                var appUser = await userManager.GetUserAsync(HttpContext.User);
+                return View(appUser ?? null);
+            }
+            catch (Exception)
+            {
+                return View(null);
+            }
+        }
+    }
+}
