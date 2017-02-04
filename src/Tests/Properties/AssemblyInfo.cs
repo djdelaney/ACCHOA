e040da81229de388edecfa7b1dadb587ab2039fa
Daniel Delaney <dan@hactar.com>
2017-02-04 14:02:02 -0500

    Disable parallel test execution - pollutes in memory EF database

--- src/Tests/Properties/AssemblyInfo.cs
+++ src/Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -17,3 +18,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c6684a7e-1120-41db-bb85-6344e1a4fd6c")]
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
