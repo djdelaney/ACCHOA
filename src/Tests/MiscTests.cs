e963c4128c90642a3aec86a2ddc6043f2b96983e
Daniel Delaney <dan@hactar.com>
2017-04-13 22:41:41 -0400

    Delete test

---
+++ src/Tests/MiscTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using HOA.Controllers;
+using HOA.Model;
+using HOA.Model.ViewModel;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using HOA.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tests.Helpers;
+
+namespace Tests
+{
+    public class MiscTests : TestBase
+    {
+        [Fact]
+        public void Delete()
+        {
+            Setup("josh.rozzi@fsresidential.com");
+            
+            RedirectToActionResult result = _controller.Delete(_sub.Id) as RedirectToActionResult;
+            Assert.Equal("List", result.ActionName);
+
+            _sub = _db.Submissions
+                    .Include(s => s.Reviews)
+                    .Include(s => s.Audits)
+                    .Include(s => s.Responses)
+                    .Include(s => s.Files)
+                    .Include(s => s.StateHistory)
+                    .Include(s => s.Comments)
+                    .FirstOrDefault(s => s.Id == _sub.Id);
+            Assert.Null(_sub);
+        }
+    }
+}
